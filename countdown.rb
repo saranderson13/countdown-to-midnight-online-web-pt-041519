dd6e1f7b13091008fd72896d06211665571f2447
Sarah Anderson <saranderson13@gmail.com>
2019-04-21 00:03:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(num)
-  string = ""
   while num > 0
-    string << "#{num} SECOND(S)!"
+    "#{num} SECOND(S)!"
     num -= 1
   end
-  string << "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
